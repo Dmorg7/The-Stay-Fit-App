84a771be17e037ad12a36798f9a08eb62f92a7b6
Benjamin Daniel Morgan <benjamindanielmorgan@benjamins-mbp.home>
2020-03-18 02:07:23 +0000

    created drawer navigation

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'auth.dart';
 
+
+
+// Log Out Button 
+
 class HomePage extends StatelessWidget {
   HomePage({this.auth, this.onSignedOut});
 
@@ -16,23 +20,114 @@ class HomePage extends StatelessWidget {
     }
   }
 
-  @override 
-    Widget build(BuildContext context) {
-      return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Home'),
-          actions: <Widget>[
-            new FlatButton(
-              child: new Text('Logout', style: new TextStyle(fontSize: 17.0, color: Colors.lime)),
-              onPressed: _signedOut
-            )
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Home'),
+        actions: <Widget>[
+          new FlatButton(
+            child: new Text('Logout',
+              style: new TextStyle(fontSize: 17.0, color: Colors.lime)),
+            onPressed: _signedOut)
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: <Color>[
+                  Color(0xFF0277BD),
+                  Colors.lightBlue[500]
+                ])
+              ),
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Material(
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset('images/user.png', width: 80, height: 80),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('User', style: TextStyle(color: Colors.lightGreenAccent, fontSize: 20.0),),
+                    )
+                  ],
+                )
+              )),
+            CustomListTile(Icons.home, 'Home',()=>{}),
+
+            CustomListTile(Icons.fitness_center, 'Workouts',()=>{}),
+
+            CustomListTile(Icons.map, 'Trainers',()=>{}),
+
+            CustomListTile(Icons.person, 'Profile',()=>{}),
           ],
         ),
-        body: new Container(
-          child: new Center(
-            child: new Text('Welcome', style: new TextStyle(fontSize: 32.0)),
+      ),
+    );
+  }
+}
+
+
+
+class CustomListTile extends StatelessWidget {
+
+  IconData icon;
+  String text;
+  Function onTap;
+
+  CustomListTile(this.icon, this.text, this.onTap);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey.shade300))
+        ),
+        child: InkWell(
+          splashColor: Colors.lightGreenAccent,
+          onTap: onTap,
+          child: Container(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(icon),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(text, style: TextStyle(
+                      fontSize: 17.0
+                    ),),
+                    ),
+                  ],
+                ),
+                Icon(Icons.arrow_right)
+              ],
+            ),
           ),
         ),
-      );
-    }
+      ),
+    );
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
